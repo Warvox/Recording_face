a4661990c287c30a743143df83d97ee215611a80
warvox <lec.go@mail.ru>
2021-06-21 15:20:24 +0300

    1.1 stage to full

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,151 +1,16 @@
-//Вывод текущей даты
+const onvif = require('node-onvif');
 
-function showCurrentDate() {
-  let currentDate = new Date();
-  const month = [
-    "января",
-    "февраля",
-    "марта",
-    "апреля",
-    "мая",
-    "июня",
-    "июля",
-    "августа",
-    "сентября",
-    "октября",
-    "ноября",
-    "декабря"
-  ];
-
-  const currentDay = currentDate.getDate();
-  const currentMonth = currentDate.getMonth();
-  const currentYear = currentDate.getFullYear();
-
-  return `${currentDay} ${month[currentMonth]} ${currentYear}г.`;
-}
-
-document.querySelector(".data").innerHTML = showCurrentDate();
-
-//Выпадающее карточка пользователя
-
-// Вывод видеопотока в div
-
-if (navigator.webkitGetUserMedia != null) {
-  var options = {
-    video: true
-  };
-
-  // Запрашиваем доступ к веб-камере
-  navigator.webkitGetUserMedia(
-    options,
-    function (stream) {
-      // Получаем тег video
-      var video = document.getElementById("video-player");
-      // Включаем поток в тег video
-      video.srcObject = stream;
-    },
-    function (e) {
-      console.log("произошла ошибка");
-    }
-  );
-}
-
-// (function() {
-//   // The width and height of the captured photo. We will set the
-//   // width to the value defined here, but the height will be
-//   // calculated based on the aspect ratio of the input stream.
-
-//   var width = 320;    // We will scale the photo width to this
-//   var height = 0;     // This will be computed based on the input stream
-
-//   // |streaming| indicates whether or not we're currently streaming
-//   // video from the camera. Obviously, we start at false.
-
-//   var streaming = false;
-
-//   // The various HTML elements we need to configure or control. These
-//   // will be set by the startup() function.
-
-//   var video = null;
-//   var canvas = null;
-//   var photo = null;
-//   var startbutton = null;
-
-//   function startup() {
-//     video = document.getElementById('video');
-//     canvas = document.getElementById('canvas');
-//     photo = document.getElementById('photo');
-//     startbutton = document.getElementById('startbutton');
-
-//     navigator.mediaDevices.getUserMedia({video: true, audio: false})
-//     .then(function(stream) {
-//       video.srcObject = stream;
-//       video.play();
-//     })
-//     .catch(function(err) {
-//       console.log("An error occurred: " + err);
-//     });
-
-//     video.addEventListener('canplay', function(ev){
-//       if (!streaming) {
-//         height = video.videoHeight / (video.videoWidth/width);
-
-//         // Firefox currently has a bug where the height can't be read from
-//         // the video, so we will make assumptions if this happens.
-
-//         if (isNaN(height)) {
-//           height = width / (4/3);
-//         }
-
-//         video.setAttribute('width', width);
-//         video.setAttribute('height', height);
-//         canvas.setAttribute('width', width);
-//         canvas.setAttribute('height', height);
-//         streaming = true;
-//       }
-//     }, false);
-
-//     startbutton.addEventListener('click', function(ev){
-//       takepicture();
-//       ev.preventDefault();
-//     }, false);
-
-//     clearphoto();
-//   }
-
-//   // Fill the photo with an indication that none has been
-//   // captured.
-
-//   function clearphoto() {
-//     var context = canvas.getContext('2d');
-//     context.fillStyle = "#AAA";
-//     context.fillRect(0, 0, canvas.width, canvas.height);
-
-//     var data = canvas.toDataURL('image/png');
-//     photo.setAttribute('src', data);
-//   }
-
-//   // Capture a photo by fetching the current contents of the video
-//   // and drawing it into a canvas, then converting that to a PNG
-//   // format data URL. By drawing it on an offscreen canvas and then
-//   // drawing that to the screen, we can change its size and/or apply
-//   // other changes before drawing it.
-
-//   function takepicture() {
-//     var context = canvas.getContext('2d');
-//     if (width && height) {
-//       canvas.width = width;
-//       canvas.height = height;
-//       context.drawImage(video, 0, 0, width, height);
-
-//       var data = canvas.toDataURL('image/png');
-//       photo.setAttribute('src', data);
-//     } else {
-//       clearphoto();
-//     }
-//   }
-
-//   // Set up our event listener to run the startup process
-//   // once loading is complete.
-//   window.addEventListener('load', startup, false);
-// })();
+console.log('Start the discovery process.');
+// Find the ONVIF network cameras.
+// It will take about 3 seconds.
+onvif.startProbe().then((device_info_list) => {
+  console.log(device_info_list.length + ' devices were found.');
+  // Show the device name and the URL of the end point.
+  device_info_list.forEach((info) => {
+    console.log('- ' + info.urn);
+    console.log('  - ' + info.name);
+    console.log('  - ' + info.xaddrs[0]);
+  });
+}).catch((error) => {
+  console.error(error);
+});
--- assets/scripts/open-load.js
+++ assets/scripts/open-load.js
@@ -1,5 +1,13 @@
 //Переход в панель пользователя
 
 document.querySelector("#btn_open").onclick = function clickOpen() {
-  document.location.href = "assets/profiles/user_panel.html";
+  let loginName = document.getElementById("login").value;
+  let passName = document.getElementById("password").value;
+  if (loginName == "security34") {
+    document.location.href = "assets/profiles/user_panel.html";
+    if (passName == "admin") {
+      document.location.href = "assets/profiles/user_panel.html";
+    }} else {
+      document.getElementById("anlogon").innerHTML = "Не верный логин или пароль."
+    }
 };
